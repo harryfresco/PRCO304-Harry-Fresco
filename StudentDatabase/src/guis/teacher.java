dc834dc91cf02c2b6326c775f8d7a1225027a8eb
harryfresco <harry.fresco@students.plymouth.ac.uk>
2020-03-07 18:26:29 +0000

    Initial commit with login

---
+++ StudentDatabase/src/guis/teacher.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guis;
+
+/**
+ *
+ * @author harryfresco
+ */
+public class teacher {
+    private String teacherIDCurrent;
+    public teacher(String teacherID){
+        teacherIDCurrent = teacherID;
+    }
+    
+    public void displayName(){
+        System.out.println(teacherIDCurrent);
+    }
+
+
+    
+   
+}
